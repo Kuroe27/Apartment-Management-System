69aed0487c47f0f1f2f376a34d8654e6d4b3e2e8
Leo Mikko Uson <usonleomikko@gmail.com>
2023-12-19 23:30:26 +0800

    Refactor sign-in functionality

--- app/login/form.tsx
+++ app/login/form.tsx
@@ -1,27 +1,11 @@
-import { Database } from "@/types/database.type";
-import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
-import { revalidatePath } from "next/cache";
-import { cookies } from "next/headers";
+import { signInWithEmail } from "@/utils/actions";
 
 export default async function () {
-  async function signInWithEmail(formData: FormData) {
-    "use server";
-    const supabase = createServerActionClient<Database>({ cookies });
-
-    const { data, error } = await supabase.auth.signInWithPassword({
-      email: formData.get("email") as string,
-      password: formData.get("password") as string,
-    });
-    console.log({ data, error });
-    revalidatePath("/");
-  }
-
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
       <form
         className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-        action="/auth/sign-in"
-        method="post"
+        action={signInWithEmail}
       >
         <input type="email" name="email" />
         <input type="password" name="password" />
