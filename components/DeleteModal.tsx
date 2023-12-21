9d6e0845a8c694dfa88d866044edb28412961f00
Leo Mikko Uson <usonleomikko@gmail.com>
2023-12-21 23:20:19 +0800

    Add DeleteModal component

---
+++ components/DeleteModal.tsx
@@ -0,0 +1,52 @@
+import { deleteApartment } from "@/utils/actions";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+} from "@nextui-org/react";
+type Props = {
+  id: number;
+  isOpen: boolean;
+  onOpenChange: (isOpen: boolean) => void;
+};
+
+const EditForm = ({ id, isOpen, onOpenChange }: Props) => {
+  return (
+    <>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Modal Title
+              </ModalHeader>
+              <ModalBody>
+                <form>
+                  <ModalFooter>
+                    <Button color="danger" variant="light" onPress={onClose}>
+                      Close
+                    </Button>
+
+                    <Button
+                      onPress={() => {
+                        deleteApartment({ id });
+                        onClose();
+                      }}
+                    >
+                      Delete
+                    </Button>
+                  </ModalFooter>
+                </form>
+              </ModalBody>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default EditForm;
